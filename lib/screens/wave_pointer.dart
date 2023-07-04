070c9bf594377ef50532a51c05572b91160f81e8
krote <krote0924@gmail.com>
2023-07-04 23:54:20 +0900

    add wave canpas ver1

---
+++ lib/screens/wave_pointer.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class WavePointer extends CustomPainter{
+    WavePointer(this.wave_src, this.color, this.constraints);
+
+    final _heightOffset = 0.25;
+    BoxConstraints constraints;
+    List<double> wave_src;
+    List<double> points;
+    Color color;
+
+    Size size;
+
+    // Set max val possible in stream, dependeing on the config
+    final absMax = 30;
+    
+    @override
+    void paint(Canvas canvas, Size size){
+        // 色、太さ、塗りつぶしの有無などを指定
+        final paint = Paint()
+        ..color = color
+        ..strokeWidth = 1.0
+        ..style = PaintingStyle.stroke;
+
+        // 得られたデータをオフセットのリストに変換する
+        points = toPoints(wave_src);
+
+        // addPolygon で path をつくり drawPath でグラフを表現する
+        final path = Path()..addPolygon(points, false);
+        canvas.drawPath(path, paint);
+    }
+
+    @override
+    bool shouldRepaint(CustomPainter oldPainting) => true;
+
+    // 得られたデータを等間隔に並べていく
+    List<Offset> toPaints(List<double> src){
+        final points = <Offset>[];
+        for(var i = 0; i< (src.length/2); i++){
+            points.add(
+                Offset(
+                    i / (src.length / 2) * constraints.maxWidth,
+                    project(src[i]), absMax, constraints.maxHeight),
+                ),
+            );
+        }
+        return points;
+    }
+
+    double project(double val, int max, double height){
+        final waveHeight = (val/max) * _heightOffset * height;
+        return waveHeight + _heightOffset * height;
+    }
+}
