28202481a11d6e56345024ffc5778ddefd162763
krote <krote0924@gmail.com>
2023-07-05 23:44:08 +0900

    add analysis models

--- lib/models/analysis_model.dart
+++ lib/models/analysis_model.dart
@@ -1,2 +1,58 @@
 import 'dart:async';
 import 'dart:math';
+
+import 'package:fft/fft.dart';
+import 'package:flutter/material.dart';
+import 'package:audio_streamer/audio_streamer.dart';
+import 'package:dart_numerics/dart_numerics.dart';
+import 'package:flutter/services.dart';
+
+class AnalysisModel extends ChangeNotifier {
+    final _streamer = AudioStreamer();
+    final _windowLength = pow(2, 15).toInt();
+    bool isRecording = false;
+    List<double> audio = List<double>.filled(pow(2, 15).toInt(), 0);
+    List<double> spectrum = List<double>.filled(pow(2,15) ~/ 2, 0);
+
+    void onAudio(List<double> buffer){
+        // fft は 2の累乗しか受け付けないため0埋めする
+        for(var i = 0; i < buffer.length; i++){
+            audio[i] = buffer[i];
+        }
+
+        // 窓掛け処理を行う
+        final window = Window(WindowType.HAMING);
+        final windowed = window.apply(audio);
+
+        // フーリエ変換
+        final fft = FFT().Transform(windowed);
+        for(var i = 0; i < _windowLength / 2; i++){
+            // パワースペクトルを dB 単位に変換
+            final tmpPower = (fft[i] * fft[i].conjugate).real / _windowLength;
+            spectrum[i] = - (10 * log10(tmpPower)); // canvasが下方向に正のため反転している
+        }
+        notifyListerners();
+    }
+    
+    void handleError(PlatformException error){
+        isRecording = false;
+        print(error.message);
+        print(error.details);
+    }
+
+    Future<void> start() async{
+        try{
+            await _streamer.start(onAudio, handleError);
+            isRecording = true;
+        }catch(error){
+            print(error);
+        }
+        notifyListerners();
+    }
+
+    Future<void> stop() async{
+        final stopped = await _streamer.stop();
+        isRecording = stopped;
+        notifyListerners();
+    }
+}
