d747f06ef64cb79d2819f84975655108c47036ce
krote <krote0924@gmail.com>
2023-07-14 23:10:52 +0900

    add menu component

--- lib/components/t_home_menu.dart
+++ lib/components/t_home_menu.dart
@@ -8,6 +8,14 @@ class THomeMenuTile extends StatelessWidget {
 
   final String menuID;
 
+  final Map<String, Map<String, String>> homeMenu = const {
+    '01': {'title': 'メニュー1', 'message': 'hello'},
+    '02': {'title': 'メニュー2', 'message': 'hi'},
+    '03': {'title': 'メニュー3', 'message': 'goood'},
+    '04': {'title': 'メニュー4', 'message': 'morning'},
+    '05': {'title': 'メニュー5', 'message': 'evening'}
+  };
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,11 +25,22 @@ class THomeMenuTile extends StatelessWidget {
         color: Colors.amber,
       ),
       child: GridTile(
-        footer: Center(
-          child: Text('${menuID}'),
-        ),
-        child: Icon(Icons.map),
-      ),
+          footer: Center(
+            child: Text(getMenuTitle(menuID)),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              //
+            },
+            child: Icon(Icons.map),
+          )),
     );
+  }
+
+  String getMenuTitle(String menuId) {
+    final Map<String, String> menuDetail =
+        homeMenu[menuId] ?? {'title': 'nothing'};
+    final String title = menuDetail['title'] ?? '';
+    return title;
   }
 }
