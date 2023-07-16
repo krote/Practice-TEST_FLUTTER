407491428d1e10df7b4e2bca9cc64b1452d8f331
krote <krote0924@gmail.com>
2023-07-16 22:56:55 +0900

    add grpc server call

--- lib/components/interview_grpc_call.dart
+++ lib/components/interview_grpc_call.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:grpc/grpc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:interview_grpc_gen/interview_grpc_gen.dart';
@@ -18,4 +19,26 @@ final channelProvider = Provider.autoDispose<ClientChannel>((ref) {
     channel.shutdown();
   });
   return channel;
+});
+
+final nameProvider = StateProvider.autoDispose<String>((ref) {
+  return '';
+});
+
+final interviewProvider = FutreProvider.autoDispose<String>((ref) async {
+  final channel = ref.watch(channelProvider);
+
+  final name = ref.watch(nameProvider);
+  if (name.isEmpty) {
+    return '';
+  }
+
+  final client = InterviewClient(channel);
+
+  final response = await client.addInterviewLog(
+    InterviewLog(),
+    options: CallOptions(compression: const GzipCodec()),
+  );
+
+  return response.message;
 });
